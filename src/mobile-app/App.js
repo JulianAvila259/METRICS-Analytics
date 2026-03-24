4e5b9168cd8275c3b70f5a8c2e3f9d98cf924039
Julian Avila <julian.avila.967@gmail.com>
2026-03-23 21:33:06 -0500

    refactor: corregir versiones de react, configurar index.js y diseño de login premium

--- src/mobile-app/App.js
+++ src/mobile-app/App.js
@@ -1,9 +1,5 @@
-import { AppRegistry } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
 
 export default function App() {
   return <LoginScreen />;
 }
-
-
-AppRegistry.registerComponent('main', () => App);
